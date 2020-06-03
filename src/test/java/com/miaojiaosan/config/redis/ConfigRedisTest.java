2f011669a75d14d68379f91eb5ccd9660401afa1
liyufei <liyufei.lyf@sunyur.com>
2020-06-03 09:27:48 +0800

    break

--- src/test/java/com/miaojiaosan/config/redis/ConfigRedisTest.java
+++ src/test/java/com/miaojiaosan/config/redis/ConfigRedisTest.java
@@ -2,12 +2,16 @@ package com.miaojiaosan.config.redis;
 
 import com.miaojiaosan.config.redis.app.Application;
 import com.miaojiaosan.config.redis.app.SpringBean;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import redis.clients.jedis.Jedis;
+
+import java.util.HashMap;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
@@ -18,14 +22,25 @@ public class ConfigRedisTest {
 
 
 
+  public static void main(String[] args) {
+    Jedis jedis = new Jedis("172.26.108.111",6379);
+    jedis.auth("shangyue#@20181010");
+    jedis.del("test");
+    jedis.hset("test",new HashMap<String, String>(){{
+      put("spring.application.name","source_web");
+    }});
+    jedis.close();
+  }
+
   @Test
   public void test() throws InterruptedException {
     long time = System.currentTimeMillis();
     while (true) {
       context.getBean(SpringBean.class);
-      if(System.currentTimeMillis() - time > 120000)
+      if(System.currentTimeMillis() - time > 60000){
+        break;
+      }
       Thread.sleep(10000);
     }
   }
-
 }
